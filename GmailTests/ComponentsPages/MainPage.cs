d280eafc76e983ae4d65e68ac08e4457fab71936
Martin Maiochi <martinmaiochi@gmail.com>
2020-06-09 18:18:38 +0100

    new components

--- GmailTests/ComponentsPages/MainPage.cs
+++ GmailTests/ComponentsPages/MainPage.cs
@@ -17,6 +17,8 @@ namespace GmailTests.ComponentsPages
 		//Buttons//
 		public IWebElement ComposeButton => _driver.FindElement(By.CssSelector(".aic .z0 div"));
 		public IWebElement SendButton => _driver.FindElement(By.XPath(".//*[contains(@class, 'T-I') and contains(@class, 'J-J5-Ji') and contains(@class, 'aoO')]"));
+		public IWebElement AttachFileInput => _driver.FindElement(By.CssSelector(".bAK input[type=\"file\"]"));
+		public IWebElement InboxButton => _driver.FindElement(By.CssSelector("[data-tooltip=\"Inbox\"] .bzz .aio .nU a[title=\"Inbox\"]"));
 		public IWebElement BinButton => _driver.FindElement(By.CssSelector("[data-tooltip=\"Bin\"] .bzz .aio .nU a[title=\"Bin\"]"));
 		public IWebElement StarredButton => _driver.FindElement(By.CssSelector("[data-tooltip=\"Starred\"] .TN .aio .nU a[title=\"Starred\"]"));
 		public IWebElement StarredEmailButton => _driver.FindElement(By.CssSelector("table[role =\"grid\"] tr:first-of-type td.apU span")); //starred option for the first mail of the list
